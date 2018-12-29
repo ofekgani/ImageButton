c204baea4e39272f61de7711e830c7a6162fd48b
ofekgani <44049881+ofekgani@users.noreply.github.com>
2018-12-29 13:09:18 +0200

    finel

--- app/src/main/java/com/example/ofekg/imagebutton/MainActivity.java
+++ app/src/main/java/com/example/ofekg/imagebutton/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.ofekg.imagebutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageButton;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    public ImageView imageView;
+    public ImageButton imageButton;
+    int num = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageButton = findViewById(R.id.imgButton);
+        imageView = findViewById(R.id.imgView);
+    }
+
+    public void go(View view) {
+        Random rnd = new Random();
+        num = rnd.nextInt(3) + 1;
+        switch (num) {
+            case 1: {
+                imageView.setImageResource(R.drawable.number1);
+                imageButton.setBackgroundResource(R.drawable.photo2);
+                break;
+            }
+            case 2: {
+                imageView.setImageResource(R.drawable.number2);
+                imageButton.setBackgroundResource(R.drawable.photo3);
+                break;
+            }
+            case 3: {
+                imageView.setImageResource(R.drawable.number3);
+                imageButton.setBackgroundResource(R.drawable.photo4);
+                break;
+
+            }
+        }
     }
 }
